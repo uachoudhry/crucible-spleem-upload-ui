ded3e5ee1fe54f29d142430e39bda9f6833294d4
Morgan Wall <mkywall3@gmail.com>
2026-04-08 16:20:01 -0700

    windows start bat with prefect

--- start.bat
+++ start.bat
@@ -1,5 +1,16 @@
+@echo off
+set PREFECT_API_URL=http://127.0.0.1:4200/api
 
-call "c:\programdata\anaconda3\condabin\conda.bat" activate  
-cd /d "%~dp0"
-uv run python main.py
-pause
+:: Start Prefect server in the background
+start /b uv run prefect server start
+timeout /t 3 /nobreak >nul
+
+:: Start flow server in the background
+start /b uv run python serve_flows.py
+
+:: Start the Flask app (foreground, Ctrl+C won't prompt Y/N)
+cmd /c uv run python main.py
+
+:: Clean up on exit (always runs after main.py stops)
+taskkill /f /im prefect.exe 2>nul
+taskkill /f /im python.exe /fi "WINDOWTITLE eq serve_flows*" 2>nul
