f72b7df334dd70550891495df73bf5c93a1d4272
Usama Choudhry <uchoudhry@lbl.gov>
2026-05-20 13:12:19 -0700

    fix deployment name mismatch in INSTRUMENT_FLOWS

--- instrument_conf.py
+++ instrument_conf.py
@@ -5,7 +5,7 @@ DEFAULT_INSTRUMENT_NAME = "qspleem_microscope"
 PRINT_BARCODE_ENABLED = False
 
 INSTRUMENT_FLOWS = {
-    "qspleem_microscope": "spleem-session-upload/spleem-session-upload",
+    "qspleem_microscope": "tem-session-upload/spleem-session-upload",  # flow-name/deployment-name: flow name matches upstream function tem_session_upload
 }
 
 ACCEPTABLE_FILE_TYPES = {'.h5', '.tif', '.tiff', '.png', '.csv', '.txt'}
